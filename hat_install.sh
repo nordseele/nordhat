a1bdaff7ea7487fdf0e84c44a2caf501dc6bad9d
nordseele <matt@nordseele.com>
2019-04-11 20:08:11 +0200

    modified script again

--- hat_install.sh
+++ hat_install.sh
@@ -1,9 +1,9 @@
-cp -f setup.sh /home/we/norns-image
-cp -f norns-matron.service /home/we/norns-image/config
-cp -f init-norns.sh /home/we/norns-image/scripts
-cp -f norns-init.service /etc/systemd/system/
-cp -f norns.target /home/we/norns-image/config
+sudo cp -f setup.sh /home/we/norns-image
+sudo cp -f norns-matron.service /home/we/norns-image/config
+sudo cp -f init-norns.sh /home/we/norns-image/scripts
+sudo cp -f norns-init.service /etc/systemd/system/
+sudo cp -f norns.target /home/we/norns-image/config
 
-cp -f raspi-blacklist.conf /etc/modprobe.d
-cp -f asound.conf /etc
-cp -f alsa.conf /usr/share/alsa
+sudo cp -f raspi-blacklist.conf /etc/modprobe.d
+sudo cp -f asound.conf /etc
+sudo cp -f alsa.conf /usr/share/alsa
