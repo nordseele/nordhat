f7d802e8181c18c143df1a43112c9c967efbfeaf
nordseele <matt@nordseele.com>
2019-04-12 14:44:10 +0200

    modified instructions

--- hat_install.sh
+++ hat_install.sh
@@ -7,3 +7,9 @@ sudo cp -f norns.target /home/we/norns-image/config
 sudo cp -f raspi-blacklist.conf /etc/modprobe.d
 sudo cp -f asound.conf /etc
 sudo cp -f alsa.conf /usr/share/alsa
+
+sudo cp -f matron.sh /home/we/norns
+
+# compile the overlays (buttons and encoders + ssd1322)
+sudo dtc -W no-unit_address_vs_reg -@ -I dts -O dtb -o /boot/overlays/norns-buttons-encoders.dtbo /home/we/nornsHat_install/overlays/norns-buttons-encoders-overlay.dts
+sudo dtc -W no-unit_address_vs_reg -@ -I dts -O dtb -o /boot/overlays/ssd1322-spi.dtbo /home/we/nornsHat_install/overlays/ssd1322-spi-overlay.dts
---
+++ pi_config.sh
@@ -0,0 +1,1 @@
+#sudo raspi-config --expand-rootfs
