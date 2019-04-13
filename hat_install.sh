9fab45832aa4bd05e55e8db7efbc43f451b4cdc2
nordseele <matt@nordseele.com>
2019-04-13 14:58:33 +0200

    mod

--- hat_install.sh
+++ hat_install.sh
@@ -1,17 +1,17 @@
 cd /home/we
 git clone https://github.com/monome/norns-image.git
 
-sudo cp -f setup.sh /home/we/norns-image
-sudo cp -f norns-matron.service /home/we/norns-image/config
-sudo cp -f init-norns.sh /home/we/norns-image/scripts
-sudo cp -f norns-init.service /etc/systemd/system/
-sudo cp -f norns.target /home/we/norns-image/config
+sudo cp -f /home/we/nornsHat_install/setup.sh /home/we/norns-image
+sudo cp -f /home/we/nornsHat_install/norns-matron.service /home/we/norns-image/config
+sudo cp -f /home/we/nornsHat_install/init-norns.sh /home/we/norns-image/scripts
+sudo cp -f /home/we/nornsHat_install/norns-init.service /etc/systemd/system/
+sudo cp -f /home/we/nornsHat_install/norns.target /home/we/norns-image/config
 
-sudo cp -f raspi-blacklist.conf /etc/modprobe.d
-sudo cp -f asound.conf /etc
-sudo cp -f alsa.conf /usr/share/alsa
+sudo cp -f /home/we/nornsHat_install/raspi-blacklist.conf /etc/modprobe.d
+sudo cp -f /home/we/nornsHat_install/asound.conf /etc
+sudo cp -f /home/we/nornsHat_install/alsa.conf /usr/share/alsa
 
-sudo cp -f matron.sh /home/we/norns
+sudo cp -f /home/we/nornsHat_install/matron.sh /home/we/norns
 
 # compile the overlays (buttons and encoders + ssd1322)
 sudo dtc -W no-unit_address_vs_reg -@ -I dts -O dtb -o /boot/overlays/norns-buttons-encoders.dtbo /home/we/nornsHat_install/overlays/norns-buttons-encoders-overlay.dts
